91b58682d568948ab9a56297f8574202ae8e2f23
Mingze Zhu <zippo1908@zippo1908s-macbook-pro.local>
2017-12-23 17:29:05 -0500

    Initial Commit

---
+++ gameofchats/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  gameofchats
+//
+//  Created by zippo1908 on 2017/12/23.
+//  Copyright © 2017年 zippo1908. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
